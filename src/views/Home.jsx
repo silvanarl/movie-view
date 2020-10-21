7d15b5e3ec3516f8160d7ab8105b2d1b39f4666e
silvanarl <ramossilvana495@gmail.com>
2020-10-21 17:10:17 -0500

    primer render
    logre renderizar las portadas despues de muchos lios

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,20 +1,21 @@
 import React,  { useState, useEffect } from 'react';
+import Movie from './Movie';
 import { 
     getPopularMovies,
 } from '../API/get';
 
 const Home = () => {
-    const [popMovie, setPopMovie] = useState({});
+    const [popMovie, setPopMovie] = useState([]);
     useEffect(() => {
-        (async()=>{
-            const arr = await getPopularMovies(setPopMovie);
-            console.log(arr.results);
-         })();
+        getPopularMovies().then((obj) => {
+            console.log(obj.results);
+            if(obj.results) setPopMovie(obj.results)
+        });
     }, []);
+    console.log(popMovie)
     return (
         <div>
-            {console.log(popMovie)}
-            Home
+            {popMovie.length > 0 && <Movie popMovie={popMovie} />}
         </div>
     )
 }
--- src/views/Movie.jsx
+++ src/views/Movie.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import {
+    IMAGE_BASE_URL,
+    BACKDROP_SIZE,
+    POSTER_SIZE
+} from '../config';
 
-const Movie = () => {
+const Movie = ({popMovie}) => {
     return (
         <div>
-            Movie
+            {popMovie !== undefined && popMovie.map((obj) => (
+                // JSON.stringify(obj)
+                <div>
+                    <p>{obj.title}</p>
+                    <img src={`${IMAGE_BASE_URL}${POSTER_SIZE}${obj.backdrop_path}`} alt={obj.title}/>
+                </div>
+            ))}
+            
         </div>
     )
 }
