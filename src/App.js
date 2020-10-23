2735bb77d7005a324b859324d87e680793cfab3e
silvanarl <ramossilvana495@gmail.com>
2020-10-23 02:11:44 -0500

    enrutado dinamico
    y padre de demas componentes, envia props a todos los hijos

--- src/App.js
+++ src/App.js
@@ -1,36 +1,43 @@
 import React, { useState, useEffect }from 'react';
 import './sass/index.scss';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './views/Home';
 import Header from './components/Header';
 import Movie from './views/Movie';
+import Footer from './components/Footer';
 import { 
   getPopularMovies,
 } from './API/get';
 
 const App = () => {
   const [popMovie, setPopMovie] = useState([]);
+  const [initialPopMovie, setInitialPopMovie] = useState([]);
     
     useEffect(() => {
         getPopularMovies().then((obj) => {
-            if(obj.results) setPopMovie(obj.results)
+            if(obj.results) setPopMovie(obj.results);
+            if(obj.results) setInitialPopMovie(obj.results);
         });
     }, []);
   return (
     <Router>
-      <div>
+      <div className="general-bgc-col">
         <header className="header-content">
           <Header />
         </header>
-        <Switch>
-          <Route exact path="/">
-          {popMovie.length > 0 && <Home popMovie={popMovie}  />}
-          </Route>
-          <Route exact path="/:id">
-          {popMovie.length > 0 && <Movie popMovie={popMovie}  />}
-          </Route>
-          {/* <Redirect to="/" /> */}
-        </Switch>
+        <main>
+          <Switch>
+            <Route exact path="/">
+            {popMovie.length > 0 && <Home popMovie={popMovie} initialPopMovie={initialPopMovie} setPopMovie={setPopMovie} />}
+            </Route>
+            <Route exact path="/:id">
+            {popMovie.length > 0 && <Movie popMovie={popMovie}  />}
+            </Route>
+          </Switch>
+        </main>
+        <footer>
+          <Footer />
+        </footer>
       </div>
     </Router>
   );
