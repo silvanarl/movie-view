08b1aa19dead62d08a5556587ac2085fee0bb909
silvanarl <ramossilvana495@gmail.com>
2020-12-13 23:37:47 -0500

    mejora de la estructura para agregar estilos

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from "prop-types";
 import { Link } from 'react-router-dom';
 import {
     IMAGE_BASE_URL,
@@ -11,12 +12,17 @@ const Movies = ({popMovie}) => {
             <h1 className="w100">Popular Movies</h1>
             {/* Preguntar si popMovie no es undefined, si es true el op && incluye el map siguiente */}
             {popMovie !== undefined && popMovie.map((obj) => (
-                <div key={obj.id} className="container-movie">
-                    <Link to={`/${obj.id}`}>
-                        <img className="container-movie-image" src={`${IMAGE_BASE_URL}${SMALL_POSTER_SIZE}${obj.poster_path}`} alt={obj.title}/>
-                    </Link>
-                    <strong><span>{obj.title}</span></strong>
-                    <span>Rated: {obj.vote_average}</span>
+                <div key={obj.id} className="flex">
+                    <div className="container-movie hvr-grow-rotate">
+                        {console.log(obj)}
+                        <Link to={`/${obj.id}`}>
+                            <img className="container-movie-image" src={`${IMAGE_BASE_URL}${SMALL_POSTER_SIZE}${obj.poster_path}`} alt={obj.title}/>
+                        </Link>
+                        <div className="container-movie-info">
+                            <h3>{obj.title}</h3>
+                            <p>⭐{obj.vote_average}</p>
+                        </div>
+                    </div>
                 </div>
             ))}
             
@@ -24,4 +30,7 @@ const Movies = ({popMovie}) => {
     )
 }
 
+Movies.propTypes = {
+    popMovie: PropTypes.arrayOf(PropTypes.object).isRequired
+}
 export default Movies;
