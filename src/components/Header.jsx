0712ce7001be24f3b6097a70341eed9774f520d8
silvanarl <ramossilvana495@gmail.com>
2020-12-18 20:38:31 -0500

    se agrego el icono de la aplicacion al header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import logo from '../assets/clapperboard.svg';
 
 const Header = () => (
     <>
+        <img src={logo} alt="movie view logo"/>
         <h1 className="header-title">movie view</h1>
     </>
 );
